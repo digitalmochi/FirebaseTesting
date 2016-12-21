f3ff1d5a3d1d4f7046ec93786d7b677a5847e7ec
digitalmochi <ric@digitalmochi.com>
2016-12-21 00:39:49 -0800

    Initial Commit

---
+++ FireBaseTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseTesting
+//
+//  Created by RiC! on 12/21/16.
+//  Copyright © 2016 Digital Mochi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
